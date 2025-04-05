30d90678d57ed2b5d07e6c86f932da7fffae014f
Sergio Alvarez <144191639+serskynet@users.noreply.github.com>
2025-04-05 13:47:06 -0500

    Add files via upload

---
+++ scrip/loader.js
@@ -0,0 +1,5 @@
+
+    window.addEventListener("load", function() {
+        const loader = document.querySelector(".loader-container");
+        loader.classList.add("hidden"); // Oculta el loader cuando la página ha cargado
+    });
